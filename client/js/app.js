71bf28bb220210612f28476f56a8bf07f28e4b2b
Paul Adams <paul.adams@frogdesign.com>
2014-10-08 22:41:21 +0900

    ehm

--- client/js/app.js
+++ client/js/app.js
@@ -9,11 +9,14 @@ angular.module('ProxyPAC', [
   'lbServices',
   'ProxyPAC.directives',
   'ProxyPAC.controllers',
-  'ui.sortable'
+  'ui.sortable',
+  'xeditable'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/groups.html', controller: 'Groups'});
   $routeProvider.when('/group/:id', {templateUrl: 'partials/group.html', controller: 'Group'});
   $routeProvider.when('/group/:id', {templateUrl: 'partials/group.html', controller: 'Group'});
   $routeProvider.otherwise({redirectTo: '/'});
-}]);
+}]).run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -5,8 +5,6 @@
 angular.module('ProxyPAC.controllers', [])
   .controller('Groups', ['$scope', 'Group', '$routeParams', function($scope, Group, $routeParams) {
     $scope.groups = Group.find();
-
-
     $scope.sortableOptions = {
       update: function(e, ui) { 
         $scope.groups.forEach(function(e, i) {
@@ -17,40 +15,52 @@ angular.module('ProxyPAC.controllers', [])
       },
       axis: 'y'
     };
-
-
   }])
   .controller('Servers', ['$scope', 'Group', '$routeParams', function($scope, Group, $routeParams) {
-
     $scope.servers = Group.Servers({id: $routeParams.id});
-
+    $scope.editing = null;
     $scope.sortableOptions = {
       update: function(e, ui) { 
         $scope.servers.forEach(function(item, i) {
             item.order = i;
             item.$save();
         });
-
       },
       axis: 'y'
+    };
+    $scope.save = function() {
+      
+       this.server.$save(function() {
+         $scope.editing  = null;
+       });
+       
+    };
+    $scope.editable = function() {
+
+      var idx = this.server.id;
+      $scope.editing = idx;
+      
     };
 
   }])
   .controller('Addresses', ['$scope', 'Group', '$routeParams', function($scope, Group, $routeParams) {
     $scope.addresses = Group.Address({id: $routeParams.id});
-
+    $scope.$watch('addresses', function(newVal, oldVal) {
+      if (newVal !== oldVal && oldVal != undefined ) {
+        /* must be a better way to call the function */
+        $scope.addresses.forEach(function(item, i) {
+          item.$save();
+        });
+      }
+    }, true);
   }])
   .controller('Group', ['$scope', 'Group', '$routeParams', function($scope, Group, $routeParams) {
     $scope.group = Group.get({id: $routeParams.id});
-    $scope.unedit = true;
-    $scope.changeToInput = function() {
-      $scope.unedit = !$scope.unedit;
-    };
-    $scope.$watch('group.name', function(i) {
-      if (typeof(i) != "undefined") {
+
+    $scope.$watch('group.name', function(newVal, oldVal) {
+      if (newVal !== oldVal && oldVal != undefined ) {
         $scope.group.$save();
       }
-      
-    }, true);
+    });
 
   }]);
